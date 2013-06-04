5d97f3b129ba4f14c14f449552085f5ae2ca9214
Katsuyoshi Ito <kito@itosoft.com>
2013-06-04 12:11:45 +0900

    add authentication path
    When you access to '/auth' then it require basic authentication.
    And then put to '/auth_upload'.

--- web.rb
+++ web.rb
@@ -2,30 +2,62 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-=begin
-use Rack::Auth::Basic do |username, password|
-  username =~ /(foo|var)/
+helpers do
+  def protect!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    username = "foo"
+    password = "bar"
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [username, password]
+  end
 end
-=end
 
 get '/' do
-    haml :index
+  haml :index
+end
+
+get '/auth' do
+  protect!
+  haml :auth
 end
 
 # アップロードされたファイルを返す
 put '/upload' do
-p params
   if params[:file]
     content_type params[:file][:type]
     f = params[:file][:tempfile]
     f.read f.size
   end
 end
+
+put '/auth_upload' do
+  protect!
+  if params[:file]
+    content_type params[:file][:type]
+    f = params[:file][:tempfile]
+    f.read f.size
+  end
+end
+
 __END__
 @@index
 %html
   %body
     %form{:action => '/upload', :method => 'POST', :enctype => 'multipart/form-data'}
+      %input{:type => 'file',   :name => 'file'}
+      %input{:type => 'submit', :value => 'upload'}
+      %input{:type => 'hidden', :name => '_method', :value => 'put'}
+
+@@auth
+%html
+  %body
+    %form{:action => '/auth_upload', :method => 'POST', :enctype => 'multipart/form-data'}
       %input{:type => 'file',   :name => 'file'}
       %input{:type => 'submit', :value => 'upload'}
       %input{:type => 'hidden', :name => '_method', :value => 'put'}
